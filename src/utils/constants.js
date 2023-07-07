50e8bccbc3b8c25d9dcd5d08991e81c53e8024b8
Prince Rastogi <106924260+pre-Coded@users.noreply.github.com>
2023-07-07 23:07:55 +0530

    push url of client

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const DB_CONNECT = "mongodb+srv://princekr:Prince8320@cluster9714.erdawbr.mongodb.net/blog?retryWrites=true&w=majority"
-const CLIENT_URL = "http://localhost:3000";
+const CLIENT_URL = "https://keepblogging.onrender.com";
 
 const GOOGLE_CLIENT_ID = "500274296590-kgoi3l3uk41k0nklehucpgn5008jkrbo.apps.googleusercontent.com";
 const GOOGLE_CLIENT_SECRET = "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";
