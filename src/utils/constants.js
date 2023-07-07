9d479de219c5c192ce3ef273c4456bdcbbf321fa
Prince Rastogi <106924260+pre-Coded@users.noreply.github.com>
2023-07-07 23:20:28 +0530

    edited again

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ const CLIENT_URL = "https://main--timely-youtiao-3188ff.netlify.app";
 
 const GOOGLE_CLIENT_ID = "500274296590-kgoi3l3uk41k0nklehucpgn5008jkrbo.apps.googleusercontent.com";
 const GOOGLE_CLIENT_SECRET = "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";
-const GOOGLE_CALLBACK = "http://localhost:3010/api/google/callback"
+const GOOGLE_CALLBACK = `${CLIENT_URL}/api/google/callback`
 
 module.exports = {
     DB_CONNECT,
